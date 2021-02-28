9de8eb869d1935ed79b1d40c867cad431cd28f6b
terencehso <51182302+terencehso@users.noreply.github.com>
2021-02-28 15:54:54 -0800

    rudimentary scale / move along view vector

--- Superliminal Scaling (Unity)/Assets/Scripts/PickUp.cs
+++ Superliminal Scaling (Unity)/Assets/Scripts/PickUp.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    public float pickupRange = 5f; // TODO: play with this, maybe obsolete
-    public float clampFactor = .1f;
-    public float moveForce = 250f;
+    // public float pickupRange = 100f; // TODO: play with this, maybe obsolete
+    public float clampFactor = .01f;
+    public float moveForce = 150f;
 
     public Transform holdParent;
     private GameObject heldObj;
@@ -14,15 +14,15 @@ public class PickUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Pickup / Drop input
         if (Input.GetKeyDown(KeyCode.E)) // TODO: replace with mouse input?
         {
             if (heldObj == null)
             {
                 RaycastHit hit;
-                if (Physics.Raycast(transform.position,                            // ro
+                if (Physics.Raycast(transform.position,                             // ro
                                     transform.TransformDirection(Vector3.forward),  // rd
-                                    out hit,                                        // raycast out
-                                    pickupRange))                                  // range
+                                    out hit))                                       // raycast out
                 {   
                     PickupObject(hit.transform.gameObject); // layer masks? maybe rigidbody is enough
                 }
@@ -32,15 +32,23 @@ public class PickUp : MonoBehaviour
                 DropObject();
             }
         }
-
         if (heldObj != null)
         {
             MoveObject();
         }
+
+        // Push along vector input
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            // Debug.Log(Input.mouseScrollDelta.y);
+            MoveHoldParent(Input.mouseScrollDelta.y);
+        }
+        // Debug.Log(Input.mouseScrollDelta);
     }
 
     void MoveObject()
     {
+        // move object to view
         if (Vector3.Distance(heldObj.transform.position, holdParent.position) > clampFactor)
         {
             Vector3 moveDirection = (holdParent.position - heldObj.transform.position);
@@ -48,15 +56,29 @@ public class PickUp : MonoBehaviour
         }
     }
 
+    void MoveHoldParent(float dir)
+    {
+        // Debug.Log(holdParent.transform.position.z);
+        // if (holdParent.transform.position.z > .2)
+        holdParent.transform.position += transform.TransformDirection(Vector3.forward)*dir;
+
+        GameObject transRef = holdParent.transform.GetChild(0).GetChild(0).gameObject;
+        Debug.Log(transRef);
+        transRef.transform.localScale += new Vector3(dir,dir,dir) * 0.2f;
+    }
+
     void PickupObject(GameObject pickObj)
     {
         if (pickObj.GetComponent<Rigidbody>())
         {
+            // holdParent.transform.position = 
             Rigidbody objRig = pickObj.GetComponent<Rigidbody>();
             objRig.useGravity = false;
             objRig.drag = 10;   // optional
-            objRig.isKinematic = true;
+            objRig.freezeRotation = true;
+            // objRig.isKinematic = true;
 
+            holdParent.transform.position = objRig.transform.position;
             objRig.transform.parent = holdParent;
             heldObj = pickObj;
         }
@@ -67,9 +89,11 @@ public class PickUp : MonoBehaviour
         Rigidbody heldRig = heldObj.GetComponent<Rigidbody>();
         heldRig.useGravity = true;
         heldRig.drag = 1;
-        heldRig.isKinematic = false;
-
+        heldRig.freezeRotation = false;
+        // heldRig.isKinematic = false;
+        
         heldObj.transform.parent = null;
         heldObj = null;
+        holdParent.transform.position = transform.position + transform.TransformDirection(Vector3.forward)*2f;
     }
 }
