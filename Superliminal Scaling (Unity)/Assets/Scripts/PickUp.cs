ad20d136e9f97a389b3b4e5214a8fcd3baad4f1d
terencehso <51182302+terencehso@users.noreply.github.com>
2021-02-28 14:41:15 -0800

    basic pickup system implemented

---
+++ Superliminal Scaling (Unity)/Assets/Scripts/PickUp.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public float pickupRange = 5f; // TODO: play with this, maybe obsolete
+    public float clampFactor = .1f;
+    public float moveForce = 250f;
+
+    public Transform holdParent;
+    private GameObject heldObj;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) // TODO: replace with mouse input?
+        {
+            if (heldObj == null)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position,                            // ro
+                                    transform.TransformDirection(Vector3.forward),  // rd
+                                    out hit,                                        // raycast out
+                                    pickupRange))                                  // range
+                {   
+                    PickupObject(hit.transform.gameObject); // layer masks? maybe rigidbody is enough
+                }
+            }
+            else
+            {
+                DropObject();
+            }
+        }
+
+        if (heldObj != null)
+        {
+            MoveObject();
+        }
+    }
+
+    void MoveObject()
+    {
+        if (Vector3.Distance(heldObj.transform.position, holdParent.position) > clampFactor)
+        {
+            Vector3 moveDirection = (holdParent.position - heldObj.transform.position);
+            heldObj.GetComponent<Rigidbody>().AddForce(moveDirection * moveForce);
+        }
+    }
+
+    void PickupObject(GameObject pickObj)
+    {
+        if (pickObj.GetComponent<Rigidbody>())
+        {
+            Rigidbody objRig = pickObj.GetComponent<Rigidbody>();
+            objRig.useGravity = false;
+            objRig.drag = 10;   // optional
+            objRig.isKinematic = true;
+
+            objRig.transform.parent = holdParent;
+            heldObj = pickObj;
+        }
+    }
+
+    void DropObject()
+    {
+        Rigidbody heldRig = heldObj.GetComponent<Rigidbody>();
+        heldRig.useGravity = true;
+        heldRig.drag = 1;
+        heldRig.isKinematic = false;
+
+        heldObj.transform.parent = null;
+        heldObj = null;
+    }
+}
